2e6d2ddda4ce6d044b63cce502bdcf819deddbf0
J.J. Hibner <jjmhibner@gmail.com>
2023-09-10 16:42:24 -0700

    work in progress: add tasks

--- juggler/projectp/views.py
+++ juggler/projectp/views.py
@@ -8,5 +8,21 @@ def contact(request):
     return render(request, "projectp/contact.html")
 
 def projects(request):
-    return render(request, "projectp/projects.html")
+    projects = Project.objects.all() 
+    projects_list = [ ]
+    for project in projects:
+        task = project.task_set.all()  
+        projects_list.append({'project': project, 'task': task})
+
+    context= {'projects_list': projects_list}
+    return render(request, "projectp/projects.html", context)
     
+
+
+#similar to book_list, in views, "for book in books", book_list.append({'book': book,...})
+#in book_list.html, "item.book.title" is this where book defined?
+
+#sql statements modelName.objects.all() retrieve object set
+""" {% if item.task.exclude(task = False) %} if
+item.task.filter(status__contains=0) {% endif %} {% if item.task.status !=
+True%} {% endif %} """
